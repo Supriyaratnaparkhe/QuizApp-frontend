91c372da47c14f5253a9daa4a9355e1cdf8802a0
Supriyaratnaparkhe <91657954+Supriyaratnaparkhe@users.noreply.github.com>
2024-01-31 14:49:20 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { useNavigate } from "react-router-dom";
 import CreateQuizForm from "../CreateQuiz/CreateQuiz";
-const Navbar = () => {
+
+const Navbar = (props) => {
+  const [selectedButton, setSelectedButton] = useState(props.selectedButton);
   const [showCreateModal, setShowCreateModal] = useState(false);
   const { userId } = useParams();
   const navigate = useNavigate();
+
   const handleLogout = (e) => {
     e.preventDefault();
+    setSelectedButton(null); 
     localStorage.removeItem("UserName");
     navigate("/login");
   };
+
   const handleAnalytics = (e) => {
     e.preventDefault();
+    setSelectedButton("analytics");
     navigate(`/analytics/${userId}`);
   };
+
   const handleDashboard = (e) => {
     e.preventDefault();
+    setSelectedButton("dashboard");
     navigate(`/dashboard/${userId}`);
   };
 
@@ -27,10 +35,24 @@ const Navbar = () => {
         <div className={styles.quizze}>Quizzie</div>
         <div className={styles.but}>
           <div>
-            <button onClick={handleDashboard}>Dahboard</button>
+            <button
+              className={
+                selectedButton === "dashboard" ? styles.selectedButtonColor : ""
+              }
+              onClick={handleDashboard}
+            >
+              Dashboard
+            </button>
           </div>
           <div>
-            <button onClick={handleAnalytics}>Analytics</button>
+            <button
+              className={
+                selectedButton === "analytics" ? styles.selectedButtonColor : ""
+              }
+              onClick={handleAnalytics}
+            >
+              Analytics
+            </button>
           </div>
           <div>
             <button
@@ -42,10 +64,12 @@ const Navbar = () => {
             </button>
           </div>
           {showCreateModal && (
-              <CreateQuizForm userId={userId} onClose={() => setShowCreateModal(false)}/>
+            <CreateQuizForm
+              userId={userId}
+              onClose={() => setShowCreateModal(false)}
+            />
           )}
         </div>
-
         <div className={styles.logout}>
           <button onClick={handleLogout}>LogOut</button>
         </div>
